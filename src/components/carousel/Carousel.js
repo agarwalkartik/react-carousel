c86f93f521b8eddc1694ab94e829364213704966
ka <ka@s20s.net>
2021-04-18 16:02:20 +0530

    remove unused imports

--- src/components/carousel/Carousel.js
+++ src/components/carousel/Carousel.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  CarouselContainer,
   LeftIcon,
   RightIcon,
   Item,
--- src/components/carousel/Filter.js
+++ src/components/carousel/Filter.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 
 import { FilterContainer, FilterButton } from "./FilterStyles";
 
